4908f8d7d9ae7e0019114e2c0089f6385a99430c
Vinícius B. de Andrade <vinas.andrade@gmail.com>
2015-11-25 17:06:16 -0200

    Arquivo de exemplo

---
+++ example.php
@@ -0,0 +1,11 @@
+<?php
+class Palindrome
+{
+    public static function isPalindrome($str)
+    {
+        throw new Exception('Not implemented');
+    }
+}
+
+// TESTE
+echo Palindrome::isPalindrome('A grama é amarga.');
