7268a4fd71892f26cb17345bf846c25422d796b6
MarcosViper <marcos.viper@gmail.com>
2015-11-26 00:50:21 -0200

    Update example.php

--- example.php
+++ example.php
@@ -1,11 +1,59 @@
 <?php
 class Palindrome
 {
+	/*
+	 *  Fn simples para checagem de palindromo.
+	 */
     public static function isPalindrome($str)
     {
-        throw new Exception('Not implemented');
+		$str = strtolower($str);
+		$str = str_replace(' ','',$str);
+		$strWork = $str;
+		
+		$strInv = strrev($strWork);
+		if ($strWork==$strInv)
+		{
+			$result = 'This is a Palindrome';
+		} else {
+			$result = 'This is not a Palindrome';
+		}
+		
+		return $result;
     }
+
+	/*
+	 *  Fn para checagem se existe um palindromo dentro da frase, considerando apenas chars alfa.
+	 */
+    public static function isPalindromeIn($str)
+    {
+		$str = strtolower($str);
+		$str = str_replace(' ','',$str);
+		$strWork = '';
+		
+		for ($i=0;$i<strlen($str);$i++)
+		{
+			$str_char = substr($str,$i,1);
+			if (ctype_alpha($str_char))
+			{
+				$strWork .= $str_char;
+			}
+		}
+		
+		$strInv = strrev($strWork);
+		if ($strWork==$strInv)
+		{
+			$result = 'This is a Palindrome';
+		} else {
+			$result = 'This is not a Palindrome';
+		}
+		
+		return $result;
+    }
+
 }
 
+
 // TESTE
-echo isPalindrome('A grama e amarga.');
+echo Palindrome::isPalindrome('A grama e amarga.')."<br>";
+echo Palindrome::isPalindromeIn('A grama e amarga.')."<br>";
+?>
